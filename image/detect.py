0e148d2d77bf96be11f511ab1e16f296c5f6e38c
Shohei Aoki <shoaok@gmail.com>
2022-07-23 22:50:06 +0300

    center of gravity detection

--- image/detect.py
+++ image/detect.py
@@ -22,7 +22,31 @@ for f in filters:
     upper_color = np.array(f[1])
     mask = cv2.inRange(hsv, lower_color, upper_color)
     output = cv2.bitwise_and(hsv, hsv, mask = mask)
+
+    chips = output
+    chips_gray = cv2.cvtColor(chips, cv2.COLOR_BGR2GRAY)
+    chips_preprocessed = cv2.GaussianBlur(chips_gray, (5, 5), 0)
+    _, chips_binary = cv2.threshold(chips_preprocessed, 100, 255, cv2.THRESH_BINARY)
+    chips_contours, _ = cv2.findContours(chips_binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    min_chip_area = 60
+    large_contours = [cnt for cnt in chips_contours if cv2.contourArea(cnt) > min_chip_area]
+    bounding_img = np.copy(chips)
+    for contour in large_contours:
+    	rect = cv2.minAreaRect(contour)
+    	box = cv2.boxPoints(rect)
+    	box = np.int0(box)
+    	cgx = int(rect[0][0])
+    	cgy = int(rect[0][1])
+    	leftx = int(cgx - (rect[1][0]/2.0))
+    	lefty = int(cgy - (rect[1][1]/2.0))
+    	angle = round(rect[2],1)
+    	cv2.drawContours(bounding_img,[box],0,(0,0,255),2)
+    	cv2.circle(bounding_img,(cgx,cgy), 10, (255,0,0), -1)
+    	font = cv2.FONT_HERSHEY_SIMPLEX
+    	cv2.putText(bounding_img,'x='+str(round(leftx))+', y='+str(round(lefty)),(leftx,lefty), font, 0.6, (255,255,255),2,cv2.LINE_AA)
+
     plt.figure()
-    plt.imshow(cv2.cvtColor(output, cv2.COLOR_BGR2RGB))
+    plt.imshow(cv2.cvtColor(bounding_img, cv2.COLOR_BGR2RGB))
 
 plt.show()
+
